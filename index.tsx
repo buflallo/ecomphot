3e1d978c9ad0dc6661bb760a1cdecf5e4ea0f3fb
buflallo <90002041+buflallo@users.noreply.github.com>
2026-02-15 19:00:39 +0100

    Fix import path for App component

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './';
 import { i18nPromise } from './i18n';
 
 const rootElement = document.getElementById('root');
